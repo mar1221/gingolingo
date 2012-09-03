f724fe53369b1eb6ec917c54554c8055eb1ac88a
Marek Takac <takacmarek1221@gmail.com>
2012-09-04 01:07:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gingolingo::Application.config.secret_token = '677e3919c21bfeddae0c6673aebdcfe3334aba8cae040132202100b3550c6f8d93730cd051e7b0e7e5965d78f28d228963b3ead7ace6b73c0a2a80dce9c815c2'
